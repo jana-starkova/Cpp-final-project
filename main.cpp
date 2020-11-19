321a1b49856492d169551686b51f1f8c300eb948
jana-starkova <45129167+jana-starkova@users.noreply.github.com>
2020-11-19 13:59:25 +0100

    ADD open camera

--- main.cpp
+++ main.cpp
@@ -3,6 +3,8 @@
 #include "opencv2/opencv.hpp"
 #include <QApplication>
 
+using namespace cv;
+
 int main(int argc, char *argv[])
 {
     QApplication a(argc, argv);
@@ -10,8 +12,19 @@ int main(int argc, char *argv[])
     Widget w;
     w.show();
 
-    // OpenCV test
-    cv::Mat Mat;
+    cv::VideoCapture cap(0); // open the default camera
+    if(!cap.isOpened())  // check if we succeeded
+        return -1;
+
+    for(;;)
+    {
+        cv::Mat frame;
+        cap >> frame; // get a new frame from camera
+        imshow("Scary Camera", frame);
+        if(cv::waitKey(30) >= 0) break; // waits for a key event for a "delay" of 30ms
+    }
+
+    // the camera will be deinitialized automatically in VideoCapture destructor
 
     return a.exec();
 }
