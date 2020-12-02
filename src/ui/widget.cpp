e71702023c6647973c46a182012f3bf3c4518b06
jana-starkova <45129167+jana-starkova@users.noreply.github.com>
2020-12-02 16:27:22 +0100

    EDIT change QCameraView to QGraphicsView, substitude QCamera with OpenCV

--- src/ui/widget.cpp
+++ src/ui/widget.cpp
@@ -2,22 +2,186 @@
 #include "ui_widget.h"
 
 #include <QVideoWidget>
+#include <unistd.h>
 
 
 Widget::Widget(QWidget *parent) :
     QWidget(parent),
-    ui(new Ui::Widget)
+    ui(new Ui::Widget),
+    m_btnScared{false},
+    m_btnMid{false},
+    m_btnBrave{false}
 {
     ui->setupUi(this);
 
-    QCamera *camera = new QCamera(this);
+    // TODO: is this okay?
+    //Camera *camera = new Camera(this);
+    /*
+    m_camera = new Camera(this);
+    m_camera->setViewfinder(ui->cameraView);
+    m_camera->start();*/
 
-    camera->setViewfinder(ui->testWidget);
-    camera->start();
+    ui->graphicsView->setScene(new QGraphicsScene(this));
+    ui->graphicsView->scene()->addItem(&pixmap);
 
 }
 
 Widget::~Widget()
 {
+    delete m_camera;
     delete ui;
+}
+
+void Widget::on_btnScared_toggled(bool checked)
+{
+    m_btnScared = checked;
+}
+
+void Widget::on_btnMid_toggled(bool checked)
+{
+    m_btnMid = checked;
+}
+
+void Widget::on_btnBrave_toggled(bool checked)
+{
+    m_btnBrave = checked;
+}
+
+void Widget::displayScaredVid()
+{
+    using namespace cv;
+
+    if(!video.open(0))
+        return;
+
+    Mat frame;
+    while(video.isOpened())
+    {
+        video >> frame;
+        if(!frame.empty())
+        {
+            /*
+            copyMakeBorder(frame,
+                           frame,
+                           frame.rows,
+                           frame.rows,
+                           frame.cols,
+                           frame.cols,
+                           BORDER_REFLECT);*/
+
+            QImage qimg(frame.data,
+                        frame.cols,
+                        frame.rows,
+                        frame.step,
+                        QImage::Format_RGB888);
+            pixmap.setPixmap( QPixmap::fromImage(qimg.rgbSwapped()) );
+            ui->graphicsView->fitInView(&pixmap, Qt::KeepAspectRatio);
+        }
+        qApp->processEvents();
+    }
+
+}
+
+void Widget::displayMidVid()
+{
+    using namespace cv;
+
+    if(!video.open(0))
+        return;
+
+    Mat frame;
+    while(video.isOpened())
+    {
+        video >> frame;
+        if(!frame.empty())
+        {
+
+            copyMakeBorder(frame,
+                           frame,
+                           frame.rows/2,
+                           frame.rows/2,
+                           frame.cols/2,
+                           frame.cols/2,
+                           BORDER_REFLECT);
+
+            QImage qimg(frame.data,
+                        frame.cols,
+                        frame.rows,
+                        frame.step,
+                        QImage::Format_RGB888);
+            pixmap.setPixmap( QPixmap::fromImage(qimg.rgbSwapped()) );
+            ui->graphicsView->fitInView(&pixmap, Qt::KeepAspectRatio);
+        }
+        qApp->processEvents();
+    }
+
+}
+
+void Widget::displayBraveVid()
+{
+    using namespace cv;
+
+    if(!video.open(0))
+        return;
+
+    Mat frame;
+    while(video.isOpened())
+    {
+        video >> frame;
+        if(!frame.empty())
+        {
+            copyMakeBorder(frame,
+                           frame,
+                           frame.rows*2,
+                           frame.rows*2,
+                           frame.cols*2,
+                           frame.cols*2,
+                           BORDER_REFLECT);
+
+            QImage qimg(frame.data,
+                        frame.cols,
+                        frame.rows,
+                        frame.step,
+                        QImage::Format_RGB888);
+            pixmap.setPixmap( QPixmap::fromImage(qimg.rgbSwapped()) );
+            ui->graphicsView->fitInView(&pixmap, Qt::KeepAspectRatio);
+        }
+        qApp->processEvents();
+    }
+
+}
+
+void Widget::on_pushButton_clicked()
+{
+    if(m_btnScared){
+        ui->selectCheck->setText("You're a chicken");
+        displayScaredVid();
+    } else if (m_btnMid){
+        ui->selectCheck->setText("And who should know?");
+        displayMidVid();
+    } else if (m_btnBrave){
+        ui->selectCheck->setText("A hero! Let's play!");
+        displayBraveVid();
+    }
+}
+
+
+void Widget::closeEvent(QCloseEvent *event)
+{
+    QMessageBox msgBox;
+    msgBox.setText("Bye now!");
+
+    if(video.isOpened())
+    {
+        msgBox.setInformativeText("Hope you had fun, see you soon!");
+        msgBox.exec();
+        video.release();
+        event->accept();
+    }
+    else
+    {
+        msgBox.setInformativeText("Next time, you can try to play...");
+        msgBox.exec();
+        event->accept();
+    }
 }
--- src/ui/widget.h
+++ src/ui/widget.h
@@ -2,8 +2,11 @@
 #define WIDGET_H
 
 #include <QWidget>
-#include <QScopedPointer>
-#include <QCamera>
+#include <QGraphicsPixmapItem>
+#include "opencv2/opencv.hpp"
+#include <QMessageBox>
+#include <QCloseEvent>
+#include "camera.h"
 
 namespace Ui {
 class Widget;
@@ -16,10 +19,29 @@ class Widget : public QWidget
 public:
     explicit Widget(QWidget *parent = nullptr);
     ~Widget();
+    void displayScaredVid();
+    void displayMidVid();
+    void displayBraveVid();
+
+protected:
+    void closeEvent(QCloseEvent *event);
+
+private slots:
+    void on_btnScared_toggled(bool checked);
+    void on_btnMid_toggled(bool checked);
+    void on_btnBrave_toggled(bool checked);
+    void on_pushButton_clicked();
+
 
 private:
     Ui::Widget *ui;
+    Camera* m_camera;
+    QGraphicsPixmapItem pixmap;
+    cv::VideoCapture video;
 
+    bool m_btnScared;
+    bool m_btnMid;
+    bool m_btnBrave;
 };
 
 #endif // WIDGET_H
